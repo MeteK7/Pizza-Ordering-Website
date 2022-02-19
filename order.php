ab03ddfb3f7e16bc9ba6df8df5e1281f883474a7
Mete <metekaba96@gmail.com>
2022-02-19 23:49:15 +0300

    Update order.php

--- order.php
+++ order.php
@@ -4,6 +4,25 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Order</title>
+	<style type="text/css">
+		table {
+		  font-family: arial, sans-serif;
+		  border-collapse: collapse;
+		  width: 70%;
+		}
+		td, th {
+		  border: 1px solid #dddddd;
+		  text-align: left;
+		  padding: 8px;
+		}
+
+		tr:nth-child(even) {
+		  background-color: #dddddd;
+		}
+		.th-price{
+			text-align: center;
+		}
+	</style>
 </head>
 <body>
 	<div>
@@ -38,9 +57,73 @@
 
 			<br>
 
-			<div>
+			<div class="div-table-pizza">
 				<p>Pizza:</p>
-				
+				<center>
+				<table>
+					<tr>
+						<th></th>
+						<th colspan="3" class="th-price">Price</th>
+						<th colspan="2"></th>
+					</tr>
+				    <tr>
+				    <th>Pizzas</th>
+				    <th>Small</th>
+				    <th>Medium</th>
+				    <th>Large</th>
+				    <th>Number</th>
+				    <th>Availability</th>
+				    </tr>
+				    <tr>
+				    <td>Vegetrian Pizza</td>
+				    <td>15TL</td>
+				    <td>17TL</td>
+				    <td>20TL</td>
+				    <td></td>
+				    <td></td>
+				    </tr>
+				    <tr>
+				    <td>Chicken Pizza</td>
+				    <td>18TL</td>
+				    <td>20TL</td>
+				    <td>22TL</td>
+				    <td></td>
+				    <td></td>
+				  </tr>
+				  <tr>
+				    <td>Meat Pizza</td>
+				    <td>18TL</td>
+				    <td>20TL</td>
+				    <td>22TL</td>
+				    <td></td>
+				    <td></td>
+				  </tr>
+				  <tr>
+				    <td>Pepperoni Pizza</td>
+				    <td>19TL</td>
+				    <td>21TL</td>
+				    <td>23TL</td>
+				    <td></td>
+				    <td></td>
+				  </tr>
+				  <tr>
+				    <td>Mix Pizza</td>
+				    <td>20TL</td>
+				    <td>22TL</td>
+				    <td>24TL</td>
+				    <td></td>
+				    <td></td>
+				  </tr>
+				  <tr>
+				    <td>COME308 Special Pizza</td>
+				    <td>22TL</td>
+				    <td>23TL</td>
+				    <td>26TL</td>
+				    <td></td>
+				    <td></td>
+				  </tr>
+				</table>
+			</center>
 			</div>			
 		</form>
 	</div>

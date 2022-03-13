7fe1654fca3b33b0141dba96b7e7eda5d157c939
Mete <metekaba96@gmail.com>
2022-03-13 13:42:38 +0300

    tbl_time_estimated and tbl_time_unit are created.

--- pizza_ordering_website.sql
+++ pizza_ordering_website.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 11, 2022 at 08:58 PM
+-- Generation Time: Mar 13, 2022 at 10:42 AM
 -- Server version: 5.7.36
 -- PHP Version: 7.4.26
 
@@ -110,11 +110,22 @@ INSERT INTO `tbl_dessert` (`id`, `name`, `price_small`, `price_medium`, `price_l
 
 DROP TABLE IF EXISTS `tbl_discount_rate`;
 CREATE TABLE IF NOT EXISTS `tbl_discount_rate` (
-  `id` int(11) NOT NULL,
-  `price_min` decimal(10,0) NOT NULL,
-  `price_max` decimal(10,0) NOT NULL,
-  `discount` decimal(10,0) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `price_min` decimal(10,2) DEFAULT NULL,
+  `price_max` decimal(10,2) DEFAULT NULL,
+  `discount` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=102 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_discount_rate`
+--
+
+INSERT INTO `tbl_discount_rate` (`id`, `price_min`, `price_max`, `discount`) VALUES
+(2, '50.00', '50.00', '10.00'),
+(3, '51.00', '70.00', '15.00'),
+(4, '70.00', NULL, '20.00'),
+(1, '0.00', '49.00', '0.00');
 
 -- --------------------------------------------------------
 
@@ -126,7 +137,8 @@ DROP TABLE IF EXISTS `tbl_history_order`;
 CREATE TABLE IF NOT EXISTS `tbl_history_order` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_customer` int(11) NOT NULL,
-  `total_price` decimal(10,2) NOT NULL,
+  `grand_total_gross_price` decimal(10,2) NOT NULL,
+  `grand_total_price` decimal(10,2) NOT NULL,
   `discount` decimal(10,2) NOT NULL,
   `date` date NOT NULL,
   PRIMARY KEY (`id`),
@@ -194,19 +206,83 @@ DROP TABLE IF EXISTS `tbl_region`;
 CREATE TABLE IF NOT EXISTS `tbl_region` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
-  `time_estimated` int(11) NOT NULL,
-  UNIQUE KEY `id_region` (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+  `id_time_estimated` int(11) NOT NULL,
+  `id_time_unit` int(11) NOT NULL,
+  UNIQUE KEY `id_region` (`id`),
+  KEY `id_time_estimated` (`id_time_estimated`),
+  KEY `id_time_unit` (`id_time_unit`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tbl_region`
 --
 
-INSERT INTO `tbl_region` (`id`, `name`, `time_estimated`) VALUES
-(1, 'Toros University', 10),
-(2, 'Pozcu', 20),
-(3, 'Mezitli', 30),
-(4, 'Others', 45);
+INSERT INTO `tbl_region` (`id`, `name`, `id_time_estimated`, `id_time_unit`) VALUES
+(1, 'Toros University', 2, 2),
+(2, 'Pozcu', 4, 2),
+(3, 'Mezitli', 6, 2),
+(4, 'Others', 9, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_time_estimated`
+--
+
+DROP TABLE IF EXISTS `tbl_time_estimated`;
+CREATE TABLE IF NOT EXISTS `tbl_time_estimated` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `time` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_time_estimated`
+--
+
+INSERT INTO `tbl_time_estimated` (`id`, `time`) VALUES
+(1, '5.00'),
+(2, '10.00'),
+(3, '15.00'),
+(4, '20.00'),
+(5, '25.00'),
+(6, '30.00'),
+(7, '35.00'),
+(8, '40.00'),
+(9, '45.00'),
+(10, '50.00'),
+(11, '55.00'),
+(12, '60.00'),
+(13, '65.00'),
+(14, '70.00'),
+(15, '75.00'),
+(16, '80.00'),
+(17, '85.00'),
+(18, '90.00'),
+(19, '95.00'),
+(20, '100.00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_time_unit`
+--
+
+DROP TABLE IF EXISTS `tbl_time_unit`;
+CREATE TABLE IF NOT EXISTS `tbl_time_unit` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_time_unit`
+--
+
+INSERT INTO `tbl_time_unit` (`id`, `name`) VALUES
+(1, 'second(s)'),
+(2, 'min(s)'),
+(3, 'hour(s)');
 
 --
 -- Constraints for dumped tables
@@ -217,6 +293,13 @@ INSERT INTO `tbl_region` (`id`, `name`, `time_estimated`) VALUES
 --
 ALTER TABLE `tbl_history_order`
   ADD CONSTRAINT `tbl_history_order_ibfk_1` FOREIGN KEY (`id_customer`) REFERENCES `tbl_customer` (`id`);
+
+--
+-- Constraints for table `tbl_region`
+--
+ALTER TABLE `tbl_region`
+  ADD CONSTRAINT `tbl_region_ibfk_1` FOREIGN KEY (`id_time_estimated`) REFERENCES `tbl_time_estimated` (`id`),
+  ADD CONSTRAINT `tbl_region_ibfk_2` FOREIGN KEY (`id_time_unit`) REFERENCES `tbl_time_unit` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
